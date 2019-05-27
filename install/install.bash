8108e03fc23211cdea87da1013bbdab2b3791c98
Shingo Kitagawa <shingogo@hotmail.co.jp>
2019-05-27 22:45:09 +0900

    update

--- install/install.bash
+++ install/install.bash
@@ -28,26 +28,26 @@ eval mkdir -p $dotfiles_path/nvim/colors
 eval curl -s https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -o $dotfiles_path/vim/colors/molokai.vim 
 eval curl -s https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim -o $dotfiles_path/nvim/colors/molokai.vim 
 
-# ccache
-if [ ! $(which "ccache") ]; then
-  echo "[ccache] sudo apt-get install ccache"
-  sudo apt-get install -y ccache
-fi
+# # ccache
+# if [ ! $(which "ccache") ]; then
+#   echo "[ccache] sudo apt-get install ccache"
+#   sudo apt-get install -y ccache
+# fi
 
-# pip
-if [ $(dpkg -s "python-pip" &> /dev/null) ]; then
-  echo "[pip] sudo apt-get purge python-pip"
-  sudo apt-get purge python-pip
-fi
-if [ ! $(which "pip") ]; then
-  pushd $HOME/Downloads
-  if [ ! -d "$HOME/Downloads/get-pip.py" ]; then
-    wget https://bootstrap.pypa.io/get-pip.py
-  fi
-  echo "[pip] python get-pip.py"
-  sudo python get-pip.py
-  popd
-fi
+# # pip
+# if [ $(dpkg -s "python-pip" &> /dev/null) ]; then
+#   echo "[pip] sudo apt-get purge python-pip"
+#   sudo apt-get purge python-pip
+# fi
+# if [ ! $(which "pip") ]; then
+#   pushd $HOME/Downloads
+#   if [ ! -d "$HOME/Downloads/get-pip.py" ]; then
+#     wget https://bootstrap.pypa.io/get-pip.py
+#   fi
+#   echo "[pip] python get-pip.py"
+#   sudo python get-pip.py
+#   popd
+# fi
 
 # percol
 if [ ! $(which "percol") ]; then

0ba3369fb308f96c735201f6d7aef4dab54f2a83
Shingo Kitagawa <shingogo@hotmail.co.jp>
2019-12-09 19:45:00 +0900

    enable alias-completion plugin

--- install/install_bash_it.bash
+++ install/install_bash_it.bash
@@ -17,6 +17,7 @@ bash-it enable alias tmux
 bash-it enable alias vim
 
 # plugins
+bash-it enable plugin alias-completion 
 bash-it enable plugin base
 bash-it enable plugin git
 bash-it enable plugin history

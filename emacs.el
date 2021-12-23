448e47377bb62d31562c7051c7761b3b0b9794cf
Shingo Kitagawa <shingogo@hotmail.co.jp>
2021-12-23 23:35:53 +0900

    add euslime

---
+++ emacs.el
@@ -0,0 +1,6 @@
+;; euslime
+
+(add-to-list 'load-path "/opt/ros/melodic/share/euslime")
+(require 'euslime-config)
+(setq inferior-euslisp-program "roseus")
+(slime-setup '(slime-fancy slime-banner slime-repl-ansi-color))
